486cb71f9f30639324393ffe35054ddec7e8625f
Hack Reactor Students <accounts@hackreactor.com>
2014-06-16 15:23:37 -0700

    Implement app with getMessages functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,23 @@
-// YOUR CODE HERE:
+// Create Chatterbox app
 
+// create namespace for our app
+// retrieve chat messages from server
+
+var app = {};
+
+app.getMessages = function() {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    success: function(data) {
+      console.log('data: ', data);
+    }
+  });
+};
+
+$(document).ready(function () {
+  app.getMessages();
+});
+
+// ensure safe escaping for all incoming data
+// continually retrieve them in order somehow
